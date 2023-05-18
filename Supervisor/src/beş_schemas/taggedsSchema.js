cb13886cabb940510f8f2fd7e2e8148be3e527ff
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2023-05-18 21:39:57 +0300

    Beş

--- Supervisor/src/beş_schemas/taggedsSchema.js
+++ Supervisor/src/beş_schemas/taggedsSchema.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
 const client = global.client;
 const schema = Schema({guildId: { type: String, default: "" },userId: { type: String, default: "" },Data:{type: Array,default:[]}});
-client.on("messageCreate",async(m) => {if(m.content.includes(".bot") && m.author.id == "928259219038302258")return m.reply({content:`${client.emoji("emote_five") !== null ? client.emoji("emote_five") : "😉"} Merhaba Ben Beş'in Altyapısıyım.`})})
+client.on("messageCreate",async(m) => {if(m.content.includes(".bot") && m.author.id == "928259219038302258")return m.reply({content:`${client.emoji("emote_five") !== null ? client.emoji("emote_five") : "😉"} Merhaba Ben Beş'in Altyapısıyım.`})});
 module.exports = model("taggedsData", schema);
